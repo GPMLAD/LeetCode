2495187eaa9ab4dbe2f1110a280fec70e43c2bae
Giovanni Perotto <giovanniperotto@hotmail.com>
2023-11-01 10:34:34 -0300

    feat: solved problem 8 from hashTable

--- hashTable/hashTable8.ts
+++ hashTable/hashTable8.ts
@@ -1,3 +1,34 @@
 function isIsomorphic(s: string, t: string): boolean {
-  return false
+  const mapS = new Map<number, string>()
+  const mapT = new Map<string, string>()
+
+  const auxS = s.split('')
+  const auxT = t.split('')
+
+  for (let i = 0; i < auxS.length; i++) {
+    let element = auxS[i]
+    mapS.set(i, element)
+  }
+
+  for (let j = 0; j < auxT.length; j++) {
+    let element = auxT[j]
+    let isMapped = mapT.has(mapS.get(j)!)
+
+    if (isMapped) {
+      const previous = mapT.get(mapS.get(j)!)
+      if (previous != element) {
+        return false
+      }
+    } else {
+      const previousValues = [...mapT.values()]
+      if (previousValues.includes(element)){
+        return false
+      }
+      mapT.set(mapS.get(j)!, element)
+    }
+  }
+  
+  return true
 }
+
+console.log(isIsomorphic('paper', 'title'))
